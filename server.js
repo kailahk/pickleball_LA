81fc90142dac4d4a2f7723076e1ecc41d79ba37c
kailahk <kailahkorsh@me.com>
2022-10-28 10:56:57 -0700

    Connect to database

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('dotenv').config();
+require('./config/database.js');
+
 var app = express();
 
 // view engine setup
